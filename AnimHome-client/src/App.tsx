c0c73f2644805c49e11e03f6f53210f22e12ee65
alongold91 <alongold91@gmail.com>
2023-11-30 15:19:04 +0200

    install next-ui

--- AnimHome-client/src/App.tsx
+++ AnimHome-client/src/App.tsx
@@ -1,37 +1,18 @@
-import './App.css'
+import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      {/* <div style={{background: "#ffe4c4", height: "100vh", width: "100vw", border:"1px solid #4C4B57" }}> */}
-      <div className='bg-background-primary border border-solid  border-border'>
-          <p style={{color: "#6D6875"}}>text light</p>
-          <p style={{color: "#333333"}}>text primary</p>
-          <p style={{color: "#1F1F1F"}}>text dark</p>
-        <div className='grid grid-cols-3 grid-rows-2 gap-4'>
-        {/* <button style={{background: "#FFD700", color: "#6D6875", border:"1px solid #4C4B57"}}>main color light text</button> */}
-        <button className='bg-primary' style={{color: "#6D6875", border:"1px solid #4C4B57"}}>main color light text</button>
-        <button style={{background: "#FFD700", color: "#333333", border:"1px solid #d4d4d4"}}>main color primary text</button>
-        <button style={{background: "#FFD700", color: "#1F1F1F"}}>main color dark text</button>
-        <button style={{background: "#f2f5ff", color: "#6D6875", border:"1px solid #4C4B57"}}>secondary color light text</button>
-        <button style={{background: "#f2f5ff", color: "#333333", border:"1px solid #d4d4d4"}}>secondary color primary text</button>
-        <button style={{background: "#f2f5ff", color: "#1F1F1F"}}>secondary color dark text</button>
-        </div>
-        <div style={{display: "flex", gap: "20px", padding: "40px", justifyContent: "center"}}>
-          <div style={{height: "350px", width: "350px", background:"#fef9f8" }}>
-          <p style={{color: "#6D6875"}}>text light</p>
-          <p style={{color: "#333333"}}>text primary</p>
-          <p style={{color: "#1F1F1F"}}>text dark</p>
-          </div>
-          <div style={{height: "350px", width: "350px", background:"#fef9f8", border:"1px solid #d4d4d4"}}></div>
-          <div style={{height: "350px", width: "350px", background:"#fef9f8"}}></div>
-        </div>
-
-      </div>
+      <Autocomplete classNames={{
+       popoverContent: 'bg-background-primary'
+      }} label="Select an animal" className="max-w-xs">
+        <AutocompleteItem key={"key"} value={"shlomi"}>
+          dori
+        </AutocompleteItem>
+      </Autocomplete>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- AnimHome-client/tailwind.config.js
+++ AnimHome-client/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     colors: {
@@ -17,6 +20,7 @@ export default {
     },
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()]
 }
 
